a8dff2f3ca81ad4774dc7b5866c093a1c661e17b
Gaurav Sen <gaurav.s3n@gmail.com>
2021-08-13 18:02:08 +0530

    updated changes

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
    // add your config here
+   // this is to test if I can make commits
 });
 
 const auth = firebase.auth();
